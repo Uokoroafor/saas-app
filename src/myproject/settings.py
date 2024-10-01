cdb5a6457c893a9e3579e3fc11647709b99894df
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-10-01 20:44:56 +0100

    Adding DJANGO_DEBUG as an environment Variable

--- src/myproject/settings.py
+++ src/myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(+kefl8z038q@fk5o9pp1o&hp+)kuh(fe1q+4(x@api5w@u8#p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")).lower()=='true'
 
 ALLOWED_HOSTS = [
     ".railway.app",
