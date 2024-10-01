ef79d9a3726ff344999023d7976d51abf0dfc264
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-10-01 22:27:31 +0100

    Pushing database changes into Production

--- src/myproject/settings.py
+++ src/myproject/settings.py
@@ -94,6 +94,16 @@ DATABASES = {
     }
 }
 
+DATABASE_URL=config('DATABASE_URL', cast=str)
+CONN_MAX_AGE=config('CONN_MAX_AGE', default=30, cast=int)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL, 
+                                      conn_health_checks=True,
+                                      conn_max_age=CONN_MAX_AGE
+                                      )}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
