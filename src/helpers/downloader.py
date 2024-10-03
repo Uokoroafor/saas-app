37e8c71e83b05e5750f706b78a7e2ea4565949ea
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-10-03 22:30:56 +0100

    Adding whitenoise and more

---
+++ src/helpers/downloader.py
@@ -0,0 +1,20 @@
+import requests
+from pathlib import Path
+
+def download_to_local(url: str, out_path: Path, parent_mkdir: bool = True):
+    
+    if not isinstance(out_path, Path):
+        raise(ValueError(f'{out_path} is not a valid Path object'))
+    if parent_mkdir:
+        out_path.parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # Write out the file in binary mode
+        out_path.write_bytes(response.content)
+        return True
+    except requests.RequestException as e:
+        print(f'Failed to download url {url} \n {e}')
+        return False
--- src/myproject/settings.py
+++ src/myproject/settings.py
@@ -50,11 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #my-apps
-    'visits'
+    'visits',
+    'commander'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,6 +142,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
+
+# source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [STATICFILES_BASE_DIR]
+
+# output for python manage.py coolectstatic
+# local cdn
+STATIC_ROOT = BASE_DIR / 'local_cdn'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
