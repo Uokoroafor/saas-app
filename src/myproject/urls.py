13e55dbd8267f7515ee583b4020961fbf011daa4
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-10-25 22:41:58 +0100

    Refactor root URL path in myproject/urls.py

--- src/myproject/urls.py
+++ src/myproject/urls.py
@@ -21,7 +21,7 @@ from auth import views as auth_views
 from .views import home_page_view, about_view
 
 urlpatterns = [
-    path("", home_page_view, name='home'),  # Root page
+    path("", home_page_view, name="home"),  # Root page
     path("hello-world/", home_page_view),
     path("login/", auth_views.login_view),
     path("register/", auth_views.register_view),
