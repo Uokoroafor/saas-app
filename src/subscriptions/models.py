0d7443facd359d7d3eb72ec376bca8a6ca6608d7
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-10-28 22:40:21 +0000

    Fixing subscriptions group bug. After many failed attempts :(

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
-class Subscription(models.Model):
-    name = models.CharField(max_length=120)
-
-    class Meta:
-        permissions=[
+SUBSCRIPTION_PERMISSIONS = [
             ("advanced", "Advanced Permissions"), # subscriptions.advanced
             ("pro", "Pro Permissions"),  # subscriptions.pro
             ("basic", "Basic Permissions"),  # subscriptions.basic
             ("basic_ai", "Basic AI Permissions"),  # subscriptions.basic
         ]
+
+
+# Create your models here.
+class Subscription(models.Model):
+    name = models.CharField(max_length=120)
+    groups = models.ManyToManyField(Group)
+    permission=models.ManyToManyField(Permission, limit_choices_to={"content_type__app_label": "subscriptions", "codename__in":[x[0] for x in SUBSCRIPTION_PERMISSIONS]})
+
+    class Meta:
+        permissions=SUBSCRIPTION_PERMISSIONS
