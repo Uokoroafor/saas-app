31d1c74110ce07ca507555b42ab97d505ca69fdd
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-11-16 07:39:25 +0000

    Adding sample test case

--- src/commander/tests.py
+++ src/commander/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.conf import settings
+
 
 # Create your tests here.
+class DBTestCase(TestCase):
+
+    def test_db_url_connection_successful(self):
+        DATABASE_URL = settings.DATABASE_URL
+        self.assertIn("neon.tech", DATABASE_URL)
