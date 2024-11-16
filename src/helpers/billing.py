f70eba372d237f734f452a28764b6bc8ee7fe32a
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-11-16 21:54:36 +0000

    Adding github actions for testing and db cleanup.

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -6,9 +6,10 @@ from . import date_utils
 
 DJANGO_DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
 STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default="", cast=str)
+STRIPE_TEST_KEY_OVERRIDE = config("STRIPE_TEST_KEY_OVERRIDE", default=False, cast=bool)
 
-if "sk_test" in STRIPE_SECRET_KEY and not DJANGO_DEBUG:
-    raise ValueError("Invalid Stripe Key for Prod")
+if ("sk_test" in STRIPE_SECRET_KEY) and (not DJANGO_DEBUG) and (not STRIPE_TEST_KEY_OVERRIDE):
+    raise ValueError("Invalid Stripe Key for Production Setting")
 stripe.api_key = STRIPE_SECRET_KEY
 
 
