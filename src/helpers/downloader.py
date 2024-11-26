55706402f41d584226df036f72562b924935ebfc
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-11-26 06:28:07 +0000

    More docstrings and improvements

--- src/helpers/downloader.py
+++ src/helpers/downloader.py
@@ -7,6 +7,26 @@ import warnings
 def download_to_local(
     url: str, out_path: Path, parent_mkdir: bool = True
 ) -> Optional[bool]:
+    """
+
+    Downloads a file from a given URL and saves it to a local path.
+
+    Args:
+        url (str): The URL of the file to be downloaded.
+        out_path (Path): The local file path where the downloaded file will be saved.
+        parent_mkdir (bool, optional): Whether to create parent directories for `out_path`
+            if they don't already exist. Defaults to True.
+
+    Returns:
+        Optional[bool]: Returns `True` if the download is successful,
+        `False` if the download fails. Returns `None` if an error occurs
+        outside the download logic.
+
+    Raises:
+        ValueError: If `out_path` is not a valid `Path` object.
+        RuntimeWarning: If the download fails due to a request error.
+    """
+
     if not isinstance(out_path, Path):
         raise (ValueError(f"{out_path} is not a valid Path object"))
     if parent_mkdir:
