39280be2e39d9af5455022e1a2e0537ba6693cf4
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-11-28 06:43:47 +0000

    Adding docstrings and typehints to profiles

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,31 +1,61 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from typing import Any, Optional
 
 User = get_user_model()
 
 
 @login_required
-def profile_list_view(request):
+def profile_list_view(request: HttpRequest) -> HttpResponse:
+    """Renders a list of active user profiles.
+
+    This view retrieves all active users from the database and renders
+    them in the 'profiles/list.html' template. Access to the view is
+    restricted to authenticated users due to the `@login_required` decorator.
+
+    Args:
+        request (HttpRequest): The HTTP request object representing the
+        incoming client request.
+
+    Returns:
+        HttpResponse: An HTTP response object containing the rendered
+        template with the context data.
+    """
     context = {"object_list": User.objects.filter(is_active=True)}
     return render(request, "profiles/list.html", context)
 
 
-# Create your views here.
 @login_required
-def profile_detail_view(request, username=None, *args, **kwargs):
-    # profile_user_obj = User.objects.get(username=username)
+def profile_detail_view(
+    request: HttpRequest,
+    username: Optional[str] = None,
+    *args: Any,
+    **kwargs: Any,
+) -> HttpResponse:
+    """Renders the detail view of a user profile.
+
+    This view retrieves a user's profile based on the provided `username`.
+    If the `username` is not found, a 404 error is raised.
+    It also checks if the profile belongs to the currently logged-in user to determine ownership.
+
+    Args:
+        request (HttpRequest): The HTTP request object representing the incoming client request.
+        username (Optional[str], optional): The username of the profile to display.
+            Defaults to None.
+        *args (Any): Additional positional arguments passed to the view.
+        **kwargs (Any): Additional keyword arguments passed to the view.
+
+    Returns:
+        HttpResponse: An HTTP response object containing the rendered
+        'profiles/detail.html' template with context data.
+    """
     user = request.user
-    print(
-        user.has_perm("subscriptions.basic"),
-        user.has_perm("subscriptions.pro"),
-        user.has_perm("subscriptions.advanced"),
-        user.has_perm("subscriptions.basic_ai"),
-    )
 
     profile_user_obj = get_object_or_404(User, username=username)
     is_logged_in_user = profile_user_obj == user
-    # logged_in_text = f"You are indeed {username}" if is_logged_in_user else f"You are not {username}!"
+
     context = {
         "object": profile_user_obj,
         "instance": profile_user_obj,
