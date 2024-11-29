1b8d0201aed49eb21b716e9ffbe9e7945b91a61d
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-11-29 07:07:37 +0000

    Adding docstrings and typehints to dashboard views

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 @login_required
-def dashboard_view(request):
+def dashboard_view(request: HttpRequest) -> HttpResponse:
+    """Displays the main dashboard for logged-in users.
+
+    This view renders the dashboard page, which provides an overview of the user's
+    activities or other relevant information. The page is accessible only to authenticated users.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Renders the dashboard page (main.html) for the logged-in user.
+    """
     return render(request, "dashboard/main.html", {})
