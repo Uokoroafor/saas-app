bde1551f9bc2e58a522b9ff1e77b7d786eba03af
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-11-29 07:59:56 +0000

    Adding doc strings to Subscription admin

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -7,6 +7,14 @@ from .models import (
 
 
 class SubscriptionPrice(admin.TabularInline):
+    """
+    Inline model for displaying SubscriptionPrice records in the admin interface.
+
+    This class configures how SubscriptionPrice records are displayed within
+    the Subscription model's admin interface. It makes the stripe_id field read-only,
+    prevents deletion of SubscriptionPrice records, and doesn't display any extra empty forms.
+    """
+
     model = SubscriptionPrice_
     readonly_fields = ["stripe_id"]
     can_delete = False
@@ -14,10 +22,21 @@ class SubscriptionPrice(admin.TabularInline):
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for managing Subscription models.
+
+    This class configures the admin interface for the Subscription model. It includes
+    the SubscriptionPrice inline model, displays the 'name' and 'active' fields in the
+    list view, and makes the 'stripe_id' field read-only.
+    """
+
     inlines = [SubscriptionPrice]
     list_display = ["name", "active"]
     readonly_fields = ["stripe_id"]
 
 
+"""
+Register the Subscription and UserSubscription models with the Django admin site.
+"""
 admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(UserSubscription)
